56d4c3fe2a10071d32bf2b9c104338699fd5adad
singh <singh@example.com>
2025-09-18 23:49:11 -0500

    Add project files

---
+++ basics6.py
@@ -0,0 +1,48 @@
+import asyncio
+import os
+
+from autogen_agentchat.agents import AssistantAgent, UserProxyAgent
+from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.ui import Console
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.tools.mcp import StdioServerParams, McpWorkbench
+
+from openai_key import OPENAI_API_KEY
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+
+async def main():
+    filesystem_server_params = StdioServerParams( command="npx",
+                                                  args=[
+                                                      "-y",
+                                                      "@modelcontextprotocol/server-filesystem",
+                                                      "/Users/rahulshetty/agenticai/AgenticAIAutoGen"],
+                                                  read_timeout_seconds=60
+                                                  )
+
+    fs_workbench = McpWorkbench( filesystem_server_params )
+
+    async with fs_workbench as fs_wb:
+        model_client = OpenAIChatCompletionClient( model="gpt-4o" )
+
+        math_tutor = AssistantAgent( name="MathTutor", model_client=model_client, workbench=fs_wb,
+                                    system_message="You are helpful math tutor.Help the user solve math problems step "
+                                                   "by step, You have to access file system"
+                                                   "When the user says 'THANKS DONE' or similar, acknowledge and say "
+                                                   "'LESSON COMPLETE' to end session." )
+
+        user_proxy = UserProxyAgent( name="Student" )
+
+        # Create team with text termination
+        team = RoundRobinGroupChat(
+            participants=[user_proxy, math_tutor],
+            termination_condition=TextMentionTermination( "LESSON COMPLETE" )
+        )
+
+        await Console( team.run_stream( task="I need help with algebra problem. Tutor, feel free to create"
+                                             "files to help with student learning " ) )
+
+    await model_client.close()
+asyncio.run( main() )
---
+++ playwrightmcp.py
@@ -0,0 +1,49 @@
+import asyncio
+import os
+
+from autogen_agentchat.agents import AssistantAgent, UserProxyAgent
+from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.ui import Console
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.tools.mcp import StdioServerParams, create_mcp_server_session, mcp_server_tools
+from openai_key import OPENAI_API_KEY
+
+
+async def main() -> None:
+    openai_api_key = OPENAI_API_KEY
+    if not openai_api_key:
+        raise RuntimeError("OPENAI_API_KEY not set in openai_key.py.")
+    model_client = OpenAIChatCompletionClient(
+        model="gpt-5-mini",
+        parallel_tool_calls=False,
+        api_key=openai_api_key,  # type: ignore
+    )
+    params = StdioServerParams(
+        command="npx",
+        args=["@playwright/mcp@latest"],
+        read_timeout_seconds=60,
+    )
+    async with create_mcp_server_session(params) as session:
+        await session.initialize()
+        tools = await mcp_server_tools(server_params=params, session=session)
+        print(f"Tools: {[tool.name for tool in tools]}")
+
+        mcp_agent = AssistantAgent(
+            name="Assistant",
+            model_client=model_client,
+            tools=tools,  # type: ignore
+        )
+
+        user_proxy = UserProxyAgent( name="Student" )
+
+        termination = TextMentionTermination("TERMINATE")
+        team = RoundRobinGroupChat(participants=[user_proxy, mcp_agent], termination_condition=termination)
+        await Console(
+            team.run_stream(
+                task="You are a web browsing assistant"
+            )
+        )
+
+
+asyncio.run(main())
